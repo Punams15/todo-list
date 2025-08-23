bb6d42f94d2c44c43a80fac8894b765ecb1ede1e
Punams15 <keys2punam@gmail.com>
2025-08-22 19:43:15 -0500

    added  onUpdateTodo to the function prop

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,6 +1,6 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({ todoList, onCompleteTodo }) {
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
   const filteredTodoList = todoList.filter(todo => !todo.isCompleted);
 
   return todoList.length === 0 ? (       //Syntax: return condition ? (expressionIfTrue) : (expressionIfFalse);
