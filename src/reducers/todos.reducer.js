ed986067670e33d187a03a63eda72562c07e0384
Punams15 <keys2punam@gmail.com>
2025-09-20 20:31:11 -0500

    export InitialState

---
+++ src/reducers/todos.reducer.js
@@ -0,0 +1,7 @@
+// export InitialState combines all useState pieces from App
+export const initialState = {
+  todoList: [],      // previously useState([])
+  isLoading: false,  // previously useState(false)
+  isSaving: false,   // previously useState(false)
+  errorMessage: '',  // previously useState('')
+};
